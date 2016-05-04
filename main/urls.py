58bb07685d5ab94909dd305465f47c60a249b773
rodrigo valdez <rodri.valdez@gmail.com>
2016-05-04 12:40:30 -0400

    *

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     
-     url(r'^distrito/(?P<distrito_nombre>[-\w]+)/$', views.distrito, name='distrito'),
+     url(r'^(?P<distrito_nombre>[-\w]+)/$', views.distrito, name='distrito'),
     url(r'^articulo/(?P<articulo_id>[-\w]+)/$', views.articulo, name='articulo'),
     url(r'^acerca-de', views.acercaDe, name='acercaDe'),
     url(r'^quienes-somos$', views.quienesSomos, name='quienesSomos'),
