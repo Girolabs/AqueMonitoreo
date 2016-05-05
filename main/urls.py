e4f0f515232fb4db7ca16d41b89c6ad6c1519a1b
rodrigo valdez <rodri.valdez@gmail.com>
2016-05-05 09:26:22 -0400

    append Slash Working

--- main/urls.py
+++ main/urls.py
@@ -18,5 +18,5 @@ urlpatterns = [
 
 handler400 = 'views.bad_request'
 handler403 = 'views.permission_denied'
-handler404 = 'views.bad_request'
+handler404 = 'views.not_found'
 handler500 = 'views.server_error'
--- main/views.py
+++ main/views.py
@@ -72,11 +72,20 @@ def bad_request(request):
 	response = render_to_response('400.html',context_instance=RequestContext(request))
 	response.status_code = 400
 	return response
+def not_found(request):
+	response = render_to_response('400.html',context_instance=RequestContext(request))
+	response.status_code = 404
+	return response
+
+def permission_denied(request):
+	response = render_to_response('400.html',context_instance=RequestContext(request))
+	response.status_code = 403
+	return response
 
 
 def server_error(request):
 	response = render_to_response('400.html',context_instance=RequestContext(request))
-	response.status_code = 400
+	response.status_code = 500
 	return response
 
 
