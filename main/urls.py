0fdea6b9e8a6c0a1b8f6f40a5ceefb11caa84d6b
Rodrigo <rodri.valdez@gmail.com>
2016-05-10 09:53:08 -0400

    url

--- main/urls.py
+++ main/urls.py
@@ -6,19 +6,19 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     
     
-    url(r'^articulo/(?P<articulo_id>[-\w]+)', views.articulo, name='articulo'),
+    url(r'^articulo/(?P<articulo_id>[-\w]+)/$', views.articulo, name='articulo'),
     
-    url(r'^acerca-de', views.acercaDe, name='acercaDe'),
-    url(r'^quienes-somos', views.quienesSomos, name='quienesSomos'),
-      url(r'^metodologia', views.metodologia, name='metodologia'),
-       url(r'^(?P<distrito_nombre>[-\w]+)', views.distrito, name='distrito'),
+    url(r'^acerca-de/$', views.acercaDe, name='acercaDe'),
+    url(r'^quienes-somos/$', views.quienesSomos, name='quienesSomos'),
+      url(r'^metodologia/$', views.metodologia, name='metodologia'),
+       url(r'^(?P<distrito_nombre>[-\w]+)$/', views.distrito, name='distrito'),
    
 
 ]
 
 
 
-handler400 = 'views.bad_request'
+handler400 = 'views.not_found'
 handler403 = 'views.permission_denied'
-handler404 = 'views.bad_request'
+handler404 = 'views.not_found'
 handler500 = 'views.server_error'
